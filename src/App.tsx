a05343f515e46089e360a9d1217af17bbc685526
Mark <fqchenxin@gmail.com>
2023-06-04 20:37:49 +1000

    add code mirror

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { ViewUpdate } from "@codemirror/view";
+import { dracula } from '@uiw/codemirror-theme-dracula';
 
 function App() {
+  const isIFrame = (input: HTMLElement | null): input is HTMLIFrameElement =>
+    input !== null && input.tagName === 'IFRAME';
+  const onChange = React.useCallback((value: string, viewUpdate: ViewUpdate) => {
+    const liveRoom = document.getElementById('live');
+
+    if (isIFrame(liveRoom) && liveRoom.contentWindow) {
+      liveRoom.contentWindow.document.body.innerHTML = value;
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex'}}>
+      <div style={{flexBasis: '50%'}}>
+        <CodeMirror
+          value="console.log('hello world!');"
+          height="1000px"
+          extensions={[javascript({ jsx: true })]}
+          theme={dracula}
+          onChange={onChange}
+        />
+      </div>
+      <iframe id="live" src="" style={{flexBasis: '50%'}}></iframe>
     </div>
   );
 }
